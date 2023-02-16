ff08ac88d772ae7482563d0b7848c4bcaaec3328
UltraHunter2007 <92379438+UltraHunter2007@users.noreply.github.com>
2023-02-16 22:38:05 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
 //DZ1
 function filterBooks(...books) {
-    const booksWithU = [];
-    const otherBooks = [];
+    const booksWithU = []
+    const otherBooks = []
 
     for (let i = 0; i < books.length; i++) {
-        const bookTitle = books[i];
+        const bookTitle = books[i]
         if (bookTitle.includes('у')) {
-            booksWithU.push(bookTitle);
+            booksWithU.push(bookTitle)
         } else {
-            otherBooks.push(bookTitle);
+            otherBooks.push(bookTitle)
         }
     }
 
-    console.log('Книги с буквой "у":');
-    console.log(booksWithU);
+    console.log('Книги с буквой "у":')
+    console.log(booksWithU)
 
-    console.log('Остальные книги:');
-    console.log(otherBooks);
+    console.log('Остальные книги:')
+    console.log(otherBooks)
 }
 
-const books = [];
-let book;
+const books = []
+let book
 
 while (book !== null) {
-    book = prompt('Введите название книги:');
+    book = prompt('Введите название книги:')
     if (book !== null) {
-        books.push(book);
+        books.push(book)
     }
 }
 
-filterBooks(...books);
+filterBooks(...books)
 
 //DZ2
 function reverseString(str) {
-    let reversed = '';
+    let reversed = ''
     for (let i = str.length - 1; i >= 0; i--) {
-        reversed += str[i];
+        reversed += str[i]
     }
     return reversed;
 }
